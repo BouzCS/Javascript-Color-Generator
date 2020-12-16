b4264f2d83b8d90e2f9bafe3cc7fee8f916b4802
Mohamed Amine Bouzmoul <medaminebouzmoul@gmail.com>
2020-12-16 18:16:51 +0100

    2nd commit!

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-function hello() {
-    console.log('ez');
+const colorx = () => {
+    const clickOn = () => {
+        const Btn = document.querySelector(".header h1");
+        Btn.addEventListener('mousemove', () => {
+        console.log("ez");
+            document.querySelector(".header h1").classList.add("allo");
+        })
+    }
+    //showing colors grids
+    const showIn = () => {
+        const playBtn = document.querySelector(".h2-container div");
+        const colorContainer = document.querySelector(".color-container");
+        const mainUp = document.querySelector(".mainUp");
+        playBtn.addEventListener("click", () =>{
+            console.log("work0");
+            mainUp.classList.add("fadeOut");
+            colorContainer.classList.add("fadeIn");
+
+        });
+    };
+clickOn();
+showIn();
 };
+colorx();

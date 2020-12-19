29a6d22e06af17b062e24a3d4140fc34c7b9439f
Mohamed Amine Bouzmoul <medaminebouzmoul@gmail.com>
2020-12-19 12:56:10 +0100

    3rd commit! color UPDATED!

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const colorx = () => {
+    alert('If you wanna reload press the <<Color Pad.>> on TOP enjoy the app!');
     const clickOn = () => {
         const Btn = document.querySelector(".header h1");
         Btn.addEventListener('mousemove', () => {
